d5932cd962a5e2465571f6c9a759feb17c51a6b0
Prajeshwar Varadharajan <prajeshwarv382@gmail.com>
2022-11-19 22:51:30 +0530

    bugs fixed

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -207,7 +207,7 @@ def logout():
   session.pop('loggedin', None)
   session.pop('id', None)
   session.pop('username', None)
-  return render_template('register.html')
+  return render_template('login.html')
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0',debug=True)

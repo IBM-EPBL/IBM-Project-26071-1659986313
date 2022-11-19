aa7c83d28e60cb93fc131601a9f249e1aef54a38
Prajeshwar Varadharajan <prajeshwarv382@gmail.com>
2022-11-19 11:22:59 +0530

    update sendGRID.py

--- Project Development Phase/Sprint 3/sendGrid.py
+++ Project Development Phase/Sprint 3/sendGrid.py
@@ -3,10 +3,10 @@ import os
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
 sg = sendgrid.SendGridAPIClient(api_key='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')
-from_email = Email("prajeshwarv382@gmail.com")
-to_email = To("maaz19121.it@rmkec.ac.in")
+from_email = Email("praj19208.it@rmkec.ac.in")
+to_email = To("praj19208.it@rmkec.ac.in")
 subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "Look how beautifully the mail is being sent.. Hehehehe")
+content = Content("text/plain", "okay this no work")
 mail = Mail(from_email, to_email, subject, content)
 
 # Get a JSON-ready representation of the Mail object
